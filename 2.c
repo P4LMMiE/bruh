1397a4ca4af68ff7853418ddddf20662fbbb64bc
P4LMMiE <89843634+P4LMMiE@users.noreply.github.com>
2023-03-08 11:48:23 +0700

    Update 2.c

--- 2.c
+++ 2.c
@@ -4,6 +4,9 @@ int main()
 {   float a,b,c,result;
     printf("Input a b c : ");
     scanf("%f%f%f",&a,&b,&c);
+    if(a<=100&&b<=100&&c<=100)
+    {
     result = ((-b)+(sqrt(pow(b,2)-(4*a*c))))/(2*a);
     printf("Output : %.2f",result);
+    }
 }
